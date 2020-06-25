e752b4205a7ae001e246a5d3887d8863f3e468f9
Natalya Myunster <natalyamyunster@yahoo.com>
2020-06-26 00:18:27 +0200

    added filter function

--- src/components/ListUsers.jsx
+++ src/components/ListUsers.jsx
@@ -1,27 +1,26 @@
 import React, {useState} from 'react';
-import {observer} from 'mobx-react';
+import { observer } from 'mobx-react';
 import usersStore from '../models/UsersStore';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import UserRowItem from './UserRowItem';
 import Button from 'react-bootstrap/Button';
 
 const ListUsers = observer(
-    function ListUsers() {
+    function ListUsers () {
         const [firstName, setFirstName] = useState('');
         const [lastName, setLastName] = useState('');
-        const [age, setAge] = useState('');
+        const [age, setAge] = useState(0);
         const [sortOrder, setSortOrder] = useState(1);
         const [users, setUsers] = useState(usersStore.usersState);
+        const [filteredList, setFilteredList] = useState(users);
 
         // Здесь нужно реализовать функцию сортировки в таблице по заголовкам
-        console.log('[].slice.call(users):', [].slice.call(users));
-
         const handleSort = (e) => {
-            const sorted = [].slice.call(users).sort((a, b) => {
+            const sorted = [...filteredList].sort((a, b) => {
                 switch (e.target.id) {
                     case 'sort-index':
-                        if (a.uid === b.uid) { return 0; }
-                        return a.uid > b.uid ? sortOrder : sortOrder * -1;
+                        if (a.guid === b.guid) { return 0; }
+                        return a.guid > b.guid ? sortOrder : sortOrder * -1;
                     case 'sort-first-name':
                         if (a.name.first === b.name.first) { return 0; }
                         return a.name.first > b.name.first ? sortOrder : sortOrder * -1;
@@ -34,13 +33,36 @@ const ListUsers = observer(
                 }
             });
             setSortOrder(sortOrder * -1);
-            setUsers(sorted);
+            setFilteredList(sorted);
         }
 
         // Здесь нужно реализовать функцию фильтрации в таблице производя, используйте инпут
+        const handleFilter = (e) => {
 
-        const itemsList = users.slice().reduce((acc, value, index) => {
-            const userIndex = usersStore.usersState.slice().findIndex((el) => {
+            const value = e.target.value;
+            const field = e.target.name;
+
+            if(field === 'firstName') setFirstName(value);
+            if(field === 'lastName') setLastName(value);
+            if(field === 'age') setAge(value);
+
+            const filter = users.filter(user => {
+                switch (field) {
+                    case 'firstName':
+                        return user.name.first.toLowerCase().includes(value.toLowerCase());
+                    case 'lastName':
+                        return user.name.last.toLowerCase().includes(value.toLowerCase());
+                    case 'age':
+                        return user.age.toString().includes(value.toString());
+                    default:
+                        return user;
+                }
+            });
+            setFilteredList(filter);
+        }
+
+        const itemsList = [...filteredList].reduce((acc, value, index) => {
+            const userIndex = [...filteredList].findIndex((el) => {
                 return (
                     el.name.first === value.name.first &&
                     el.name.last === value.name.last
@@ -49,95 +71,78 @@ const ListUsers = observer(
 
             return userIndex === -1
                 ? acc
-                : [...acc, <UserRowItem
-                    key={`userItem_${index}`}
-                    value={value}
-                    userID={userIndex}
-                />]
+                : acc.concat([
+                    <UserRowItem
+                        key={`userItem_${index}`}
+                        value={value}
+                        userID={userIndex}
+                    />,
+                ]);
         }, []);
 
-        return (
+        return(
             <div className="userListDiv">
                 <table className="table table-striped">
                     <thead>
-                    <tr>
-                        <th>
-                            <Button
-                                variant="outline-dark"
-                                id="sort-index"
-                                onClick={handleSort}
-                            >
-                                #
-                            </Button>
-                        </th>
-                        <th>
-                            <Button
-                                variant="outline-dark"
-                                id="sort-first-name"
-                                onClick={handleSort}
-                            >
-                                First Name
-                            </Button>
-                        </th>
-                        <th>
-                            <Button
-                                variant="outline-dark"
-                                id="sort-last-name"
-                                onClick={handleSort}
-                            >
-                                Last Name
-                            </Button>
-                        </th>
-                        <th>
-                            <Button
-                                variant="outline-dark"
-                                id="sort-age"
-                                onClick={handleSort}
-                            >
-                                Age
-                            </Button>
-                        </th>
-                        <th>Actions</th>
-                    </tr>
+                        <tr>
+                            <th>
+                                <Button variant="outline-dark" id="sort-index" onClick={handleSort}>
+                                    #
+                                </Button>
+                            </th>
+                            <th>
+                                <Button variant="outline-dark" id="sort-first-name" onClick={handleSort}>
+                                    First Name
+                                </Button>
+                            </th>
+                            <th>
+                                <Button variant="outline-dark" id="sort-last-name" onClick={handleSort}>
+                                    Last Name
+                                </Button>
+                            </th>
+                            <th>
+                                <Button variant="outline-dark" id="sort-age" onClick={handleSort}>
+                                    Age
+                                </Button>
+                            </th>
+                            <th>Actions</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    <tr key="tr_filter">
-                        <td key="id_filter">-</td>
-                        <td key="nf_input">
-                            <input
-                                type="text"
-                                name="firstName"
-                                value={firstName}
-                                onChange={() => {
-                                }}
-                            />
-                        </td>
-                        <td key="nl_input">
-                            <input
-                                type="text"
-                                name="lastName"
-                                value={lastName}
-                                onChange={() => {
-                                }}
-                            />
-                        </td>
-                        <td key="age_input">
-                            <input
-                                type="number"
-                                min="1"
-                                step="1"
-                                name="age"
-                                value={age}
-                                onChange={() => {
-                                }}
-                            />
-                        </td>
-                        <td key="actoions_input">
-                            <span/>
-                            <span/>
-                        </td>
-                    </tr>
-                    {itemsList}
+                        <tr key="tr_filter">
+                            <td key="id_filter">-</td>
+                            <td key="nf_input">
+                                <input
+                                    type="text"
+                                    name="firstName"
+                                    value={firstName}
+                                    onChange={handleFilter}
+                                />
+                            </td>
+                            <td key="nl_input">
+                                <input
+                                    type="text"
+                                    name="lastName"
+                                    value={lastName}
+                                    onChange={handleFilter}
+                                />
+                            </td>
+                            <td key="age_input">
+                                <input
+                                    type="number"
+                                    min="1"
+                                    step="1"
+                                    name="age"
+                                    value={age}
+                                    onChange={handleFilter}
+                                />
+                            </td>
+                            <td key="actoions_input">
+                                <span />
+                                <span />
+                            </td>
+                        </tr>
+                        {itemsList}
                     </tbody>
                 </table>
                 <Link to="/create">
